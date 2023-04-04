f99ac9790b2e8ced8b63dd35b3a3e75b05ef21c2
ricRobo4 <artistr3@proton.me>
2023-04-04 13:56:53 -0700

    Added to HomePage layout Added homePageBg.png and Login BG.jpg to images folder Added HopePage component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
+import { Container, Row, Col } from 'react-bootstrap';
+// import logo from './logo.svg';
 import './App.css';
+import { HomePage } from './Components/Homepage';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <HomePage />
+            </Col>
+          </Row>
+        </Container>
       </div>
       <Routes>
         
--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -3,17 +3,30 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 function HomePage () 
 {
     return (
-        <div>
-            <Container>
-                <Row>
+        <>
+            <Container className="hero-bg">
+                <Row className="title-and-login-btn">
+                    <Col>
+                        <h2>GameSwap</h2>
+                    </Col>
+                    <Col className="login-btn">
+                        <Button>Login</Button>
+                    </Col>
+                </Row>
+                <Row className="header-and-description">
                     <Col>
                         <h1>Make your old games someone else's problem</h1>
                     </Col>
                     <Col>
                         <p>Swap your old games for ones that are new to you. And make new friends in the process!</p>
                     </Col>
+                    <Col>
+                        <Button>Join Now</Button>
+                    </Col>
                 </Row>
-                <Row>
+            </Container>
+            <Container>
+                <Row className="new-releases-row">
                     <Col>
                         <h1>New Releases</h1>
                     </Col>
@@ -21,6 +34,8 @@ function HomePage ()
                         {/* This col is where newly-released games will be shown */}
                     </Col>
                 </Row>
+            </Container>
+            <Container className="trending-row">
                 <Row>
                     <Col>
                         <h1>Trending</h1>
@@ -30,6 +45,8 @@ function HomePage ()
                     </Col>
                 </Row>
             </Container>
-        </div>
+        </>
     );
 }
+
+export { HomePage }
