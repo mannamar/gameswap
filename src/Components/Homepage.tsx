3c34750940a06b22d9505a25685e1eb6e9262fee
ricRobo4 <artistr3@proton.me>
2023-04-04 11:03:57 -0700

    Started building out layout for Homepage

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -1,8 +1,35 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+
 function HomePage () 
 {
     return (
         <div>
-            
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Make your old games someone else's problem</h1>
+                    </Col>
+                    <Col>
+                        <p>Swap your old games for ones that are new to you. And make new friends in the process!</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h1>New Releases</h1>
+                    </Col>
+                    <Col>
+                        {/* This col is where newly-released games will be shown */}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h1>Trending</h1>
+                    </Col>
+                    <Col>
+                        {/* This is where trending games will be shown */}
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
