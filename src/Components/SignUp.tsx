009c3e929b2c15b55fa40a5bda3cabe966f7752f
ricRobo4 <artistr3@proton.me>
2023-04-05 15:46:28 -0700

    * Added tabs for Login and Sign up forms

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import { Container, Row, Col, Button, Form, Tab, Tabs } from "react-bootstrap";
 
 function SignUp() {
     const [Name, setName] = useState('');
@@ -25,71 +25,106 @@ function SignUp() {
                             </Col>
                         </Row>
                     </Col>
-                    <Col xs={5}>
-                        <Form>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control />
-                            </Form.Group>
+                    <Col xs={5} className='form-section'>
+                    <br />
+                        <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
 
-                            <Form.Group className="mb-3">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control />
-                            </Form.Group>
+                            <Tab eventKey={1} title="Log In">
+                                <br />
+                                {/* Start of Login tab */}
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>E-Mail</Form.Label>
-                                <Form.Control />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3">
-                                <Form.Label>Date of Birth</Form.Label>
-                                <Row>
-                                    <Col xs={6}>
-                                        <Form.Select aria-label="Default select example">
-                                        <option>Select Month...</option>
-                                        <option>January</option>
-                                        <option>February</option>
-                                        <option>March</option>
-                                        <option>April</option>
-                                        <option>May</option>
-                                        <option>June</option>
-                                        <option>July</option>
-                                        <option>August</option>
-                                        <option>September</option>
-                                        <option>October</option>
-                                        <option>November</option>
-                                        <option>December</option>
-                                        </Form.Select>
-                                    </Col>
-                                    <Col xs={3}>
-                                        <Form.Control placeholder='Day'/>
-                                    </Col>
-                                    <Col xs={3}>
-                                        <Form.Control placeholder='Year'/>
-                                    </Col>
-                                </Row>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3">
-                                <Form.Label>Zip Code</Form.Label>
-                                <Form.Control />
-                            </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" />
+                                    </Form.Group>
 
 
 
-                            <div className="d-grid gap-2">
-                                <Button variant="primary">
-                                    Sign Up
-                                </Button>
-                            </div>
+                                    <div className="d-grid gap-2">
+                                        <Button variant="primary">
+                                            Log In
+                                        </Button>
+                                    </div>
 
-                        </Form>
+                                </Form>
+                                {/* End of Login tab */}
+                            </Tab>
+                            <Tab eventKey={2} title="Sign Up">
+                            <br />
+                                {/* Start of Sign up tab */}
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Name</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>E-Mail</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Date of Birth</Form.Label>
+                                        <Row>
+                                            <Col xs={6}>
+                                                <Form.Select aria-label="Default select example">
+                                                    <option>Select Month...</option>
+                                                    <option>January</option>
+                                                    <option>February</option>
+                                                    <option>March</option>
+                                                    <option>April</option>
+                                                    <option>May</option>
+                                                    <option>June</option>
+                                                    <option>July</option>
+                                                    <option>August</option>
+                                                    <option>September</option>
+                                                    <option>October</option>
+                                                    <option>November</option>
+                                                    <option>December</option>
+                                                </Form.Select>
+                                            </Col>
+                                            <Col xs={3}>
+                                                <Form.Control placeholder='Day' />
+                                            </Col>
+                                            <Col xs={3}>
+                                                <Form.Control placeholder='Year' />
+                                            </Col>
+                                        </Row>
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Zip Code</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+
+
+
+                                    <div className="d-grid gap-2">
+                                        <Button variant="primary">
+                                            Sign Up
+                                        </Button>
+                                    </div>
+
+                                </Form>
+                            {/* End of Sign Up tab */}
+                            </Tab>
+                        </Tabs>
+
                     </Col>
                 </Row>
             </Container>
