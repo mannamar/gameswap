c01d78ec3872f23bddbcddea58c26e7e53158035
Amardeep Mann <amar2slick@gmail.com>
2023-04-05 12:13:59 -0700

    API proxy working

--- src/Services/IgdbServices.ts
+++ src/Services/IgdbServices.ts
@@ -16,12 +16,13 @@ async function searchForGames(input: string) {
     console.log(input);
     console.log(clientID);
     console.log(token);
-    let res = await fetch('https://api.igdb.com/v4/games', {
+    let res = await fetch('http://localhost:8080/api.igdb.com/v4/games', {
         method: 'POST',
         headers: {'Client-ID': clientID, 'Authorization': `Bearer ${token}`},
         body: 'search "Halo"; fields name, cover.url, platforms.abbreviation, first_release_date;'
     });
-    console.log(res);
+    let data = await res.json();
+    console.log(data);
 }
 
 export { searchForGames };
