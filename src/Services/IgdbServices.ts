d5b201c1515e7f365c5141c65e713e68478a8de5
Amardeep Mann <amar2slick@gmail.com>
2023-04-09 15:56:40 -0700

    API-Proxy URL Updated

--- src/Services/IgdbServices.ts
+++ src/Services/IgdbServices.ts
@@ -16,7 +16,7 @@ async function searchForGames(input: string) {
     console.log(input);
     console.log(clientID);
     console.log(token);
-    let res = await fetch('http://localhost:8080/https://api.igdb.com/v4/games', {
+    let res = await fetch('https://gameswap-apiproxy.herokuapp.com/https://api.igdb.com/v4/games', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
