133fbb8a13813a6d4fc1ffce1eab2b6b21f69f57
ricRobo4 <artistr3@proton.me>
2023-04-12 15:52:35 -0700

    * Tried to organize match items in a row

--- src/Components/Matches.tsx
+++ src/Components/Matches.tsx
@@ -3,9 +3,9 @@ import { Container, Col, Row } from 'react-bootstrap';
 function Matches() {
     let youRecieve: string = 'Super Mario Odyssey';
     let theyRecieve: string = 'Elden Ring';
-  return (
-    <div>
-        <Container fluid className="hero-bg-matches">
+    return (
+        <div>
+            <Container fluid className="hero-bg-matches">
                 <Row className="title-and-login-btn">
                     <Col>
                         <h2>GameSwap</h2>
@@ -24,28 +24,56 @@ function Matches() {
             </Container>
             <Container>
                 <br />
-                
-                <Row className='game-trade-matches'>
-                    <Col className='game-match'>
-                        <Row>
-                            <Col xs={6} className='center-matches'>
-                                <img className='matches-game-cover' alt='Game Cover' src={require('../Assets/Images/GameCoverPlaceholders/Mario Odyssey 1.png')}/>
-                            </Col>
-                            <Col xs={6} className='center-matches'>
-                                <img className='matches-game-cover'  alt='Game Cover' src={require('../Assets/Images/GameCoverPlaceholders/Elden Ring 1.png')}/>
+                <Row className='game-matches-row'>
+                    <Col>
+                        {/* This is the start of a game trade item */}
+                        <Row className='game-trade-matches'>
+                            <Col className='game-match'>
+                                <Row>
+                                    <Col xs={6} className='center-matches'>
+                                        <img className='matches-game-cover' alt='Game Cover' src={require('../Assets/Images/GameCoverPlaceholders/Mario Odyssey 1.png')} />
+                                    </Col>
+                                    <Col xs={6} className='center-matches'>
+                                        <img className='matches-game-cover' alt='Game Cover' src={require('../Assets/Images/GameCoverPlaceholders/Elden Ring 1.png')} />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <p>You Receive: {youRecieve}</p>
+                                        <p>They Receive: {theyRecieve}</p>
+                                    </Col>
+                                </Row>
                             </Col>
                         </Row>
-                        <Row>
-                            <Col>
-                                <p>You Receive: {youRecieve}</p>
-                                <p>They Receive: {theyRecieve}</p>
+                        {/* This is the end of a game trade item */}
+                    </Col>
+                    <Col>
+                        {/* This is the start of a game trade item */}
+                        <Row className='game-trade-matches'>
+                            <Col className='game-match'>
+                                <Row>
+                                    <Col xs={6} className='center-matches'>
+                                        <img className='matches-game-cover' alt='Game Cover' src={require('../Assets/Images/GameCoverPlaceholders/Mario Odyssey 1.png')} />
+                                    </Col>
+                                    <Col xs={6} className='center-matches'>
+                                        <img className='matches-game-cover' alt='Game Cover' src={require('../Assets/Images/GameCoverPlaceholders/Elden Ring 1.png')} />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <p>You Receive: {youRecieve}</p>
+                                        <p>They Receive: {theyRecieve}</p>
+                                    </Col>
+                                </Row>
                             </Col>
                         </Row>
+                        {/* This is the end of a game trade item */}
                     </Col>
                 </Row>
+
             </Container>
-    </div>
-  )
+        </div>
+    )
 }
 
 export { Matches }
