8bf7443b3fb518d00fba2b2720b39a5a7bac3832
ricRobo4 <artistr3@proton.me>
2023-05-05 14:11:31 -0700

    * Tried to add login in Javascript for effect: Not successful so far

--- src/Components/WishListItem.tsx
+++ src/Components/WishListItem.tsx
@@ -5,12 +5,20 @@ function WishListItem() {
     let releaseYear: number = 2017;
     let platform: string = 'Nintendo Switch';
 
+    let wishlistItemClass: string = 'add-to-wishlist';
+
+    function AddItemHover () {
+        wishlistItemClass = 'add-to-wishlist-visible add-visible';
+    }
+
     return (
-        <Col xs={2} className="image-with-overlay">
+        <Col xs={2} className="image-with-overlay"
+        onMouseOver={(AddItemHover)}
+        >
             <div className="image-darken">
                 <img className="game-cover-placeholder" alt="Game cover" src={require('../Assets/Images/GameCoverPlaceholders/Mario Odyssey 1.png')} />
             </div>
-            <p className="add-to-wishlist">+ Add To Wishlist</p>
+            <p className={wishlistItemClass}>+ Add To Wishlist</p>
 
             {/* <div className="hover-overlay-effect"></div> */}
             <p>{gameTitle}</p>
