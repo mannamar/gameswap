abb9e3e535750eccb639f441f68143842399c38e
kenzodiac <kennethfujimura@gmail.com>
2023-05-15 16:30:39 -0700

    finished navbar component

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, Dropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { Gift, Handshake, ChatText } from '@phosphor-icons/react';
+import './Navbar.css';
 
 export default function Navbar(props: { signedInPass: boolean }) {
-
+    //hooks for managing the logged in user's name appearing
     const [loggedInUsername, setLoggedInUsername] = useState('');
-    useEffect (() => {
-        const data: any = localStorage.getItem('LoggedInUser');
-        const jsonData = JSON.parse(data);
-        // console.log(jsonData);
-        setLoggedInUsername(jsonData.username);
+    useEffect(() => {
+        if (localStorage.getItem('LoggedInUser')){
+            const data: any = localStorage.getItem('LoggedInUser');
+            const jsonData = JSON.parse(data);
+            // console.log(jsonData);
+            setLoggedInUsername(jsonData.username);
+        }
     })
 
+    //hook for logging out user by deleting userinfo from local storage
+    const handleDeleteItemClick = () => {
+        localStorage.removeItem('Token');
+        localStorage.removeItem('LoggedInUser');
+        window.location.reload();
+    }
+
     return (
-        <div>
+        <div className={"nav-style"}>
             {
                 !props.signedInPass ?
                     <Row className="title-and-login-btn">
-                        <Col>
-                            <h2>GameSwap</h2>
+                        <Col sm={3}>
+                            <Link className={"nav-links"} to={"/"}>
+                                <span>GameSwap</span>
+                            </Link>
                         </Col>
                         <Col className="login-btn">
                             <Link style={{ textDecoration: 'none' }} to="/Login">
@@ -30,23 +43,42 @@ export default function Navbar(props: { signedInPass: boolean }) {
                     </Row>
                     :
                     <Row className="title-and-login-btn">
-                        <Col>
-                            <h2>GameSwap</h2>
+                        <Col sm={3} className={'d-flex align-items-center'}>
+                            <Link className={"nav-links"} to={"/"}>
+                                <span>GameSwap</span>
+                            </Link>
                         </Col>
-                        <Col>
-                            <Row>
-                                <Col>Wishlist</Col>
-                                <Col>Matches</Col>
-                                <Col>Messages</Col>
-                            </Row>
-                        </Col>
-                        <Col className="login-btn">
-                            <Row>
-                                <Col>
-                                    <img src={require('../Assets/Images/GameCoverPlaceholders/amaninatubIcon.png')} />
-                                </Col>
-                                <Col>{loggedInUsername}</Col>
-                            </Row>
+
+                        <Col sm={9} className="login-btn">
+                            <div className={'d-flex align-items-center'} style={{marginRight: '15px'}}>
+                                <Link className={"nav-links"} to="/Wishlist">
+                                    <Gift size={18} className={"navbar-icons"} /> Wishlist
+                                </Link>
+                            </div>
+                            <div className={'d-flex align-items-center'} style={{marginRight: '15px'}}>
+                                <Link className={"nav-links"} to="/Matches">
+                                    <Handshake size={19} className={"navbar-icons"} /> Matches
+                                </Link>
+                            </div>
+                            <div className={'d-flex align-items-center'} style={{marginRight: '49px'}}>
+                                <Link className={"nav-links"} to="/Messages">
+                                    <ChatText size={16} className={"navbar-icons"} /> Messages
+                                </Link>
+                            </div>
+                            <div className={'d-flex align-items-center'}>
+                                <img src={require('../Assets/Images/GameCoverPlaceholders/amaninatubIcon.png')} />
+                            </div>
+                            <div style={{marginLeft: '10px'}} className={'d-flex align-items-center'} >
+                                <Dropdown>
+                                    <Dropdown.Toggle variant="none" id="dropdown">
+                                        <span style={{fontSize: '14px', fontWeight: '600'}}>{loggedInUsername}</span>
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item onClick={handleDeleteItemClick}>Logout</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+
+                            </div>
                         </Col>
                     </Row>
             }
